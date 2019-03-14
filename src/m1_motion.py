d724a7c3084bb743a579cf1292c15a922186a7ac
sorensms <sorensms@rose-hulman.edu>
2019-03-14 11:02:55 -0400

    Done

--- src/m1_motion.py
+++ src/m1_motion.py
@@ -2,11 +2,11 @@
 An opportunity to explore how to make an EV3 Robot move.
 
 Authors: Dave Fisher, David Mutchler, Vibha Alangar,
-their colleagues, and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+their colleagues, and Maddie Sorensen.
+"""  # Done: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 # -----------------------------------------------------------------------------
-# TODO: 2.
+# Done: 2.
 #   Follow along with the lecture to run this program:
 #    - Using SSH from your computer
 #   When you have successfully run this program, change this _TODO_ to DONE.
@@ -16,15 +16,16 @@ import simple_rosebotics as rb
 import time
 
 
+
 def main():
     """ Calls the other functions to test/demo them. """
     print("Running main on the robot.")
-
-    # TODO: 2. Construct a RoseBot.  Send it as an argument to other functions.
-    run_test_spin()
-    run_test_go()
-    challenge1()
-    challenge2()
+    robot=rb.RoseBot()
+    #done: 2. Construct a RoseBot.  Send it as an argument to other functions.
+    run_test_spin(robot)
+    run_test_go(robot)
+    challenge1(robot)
+    challenge2(robot)
 
 
 def run_test_spin(robot):
@@ -35,14 +36,20 @@ def run_test_spin(robot):
       :type robot:  rb.RoseBot
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement this.
+    # Done: 3. Implement this.
     # -------------------------------------------------------------------------
-
+    spin(robot,2,50)
 
 def spin(robot, seconds, speed):
+    robot.drive_system.left_motor.turn_on(speed)
+    robot.drive_system.right_motor.turn_on(-speed)
+    time.sleep(seconds)
+    robot.drive_system.left_motor.turn_off()
+    robot.drive_system.right_motor.turn_off()
+
     """ :type robot: rb.RoseBot """
     # -------------------------------------------------------------------------
-    # TODO: 4.
+    # Done: 4.
     #   Makes the robot move, by using this pattern:
     #    1. Turn on the wheel motors at the given speed but with:
     #        -- LEFT wheel POSITIVE speed
@@ -62,15 +69,20 @@ def run_test_go(robot):
       :type robot:  rb.RoseBot
     """
     # -------------------------------------------------------------------------
-    # TODO: 3. Implement this.
+    # Done: 3. Implement this.
     # -------------------------------------------------------------------------
-
+    go(robot, 5, 45,80)
 
 
 def go(robot, seconds, left_wheel_speed, right_wheel_speed):
     """ :type robot: rb.RoseBot """
+    robot.drive_system.left_motor.turn_on(left_wheel_speed)
+    robot.drive_system.right_motor.turn_on(right_wheel_speed)
+    time.sleep(seconds)
+    robot.drive_system.right_motor.turn_off()
+    robot.drive_system.left_motor.turn_off()
     # -------------------------------------------------------------------------
-    # TODO: 6.
+    # Done: 6.
     #   Make the robot go, by using the pattern from SPIN function, except
     #   using the given speeds for the left and right wheels, respectively.
     # -------------------------------------------------------------------------
